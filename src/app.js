7cfc8e56c73a9017ef05e4d7bf81968b11dab621
Abhinaw Tiwari <Abhinaw.Tiwari@ril.com>
2019-07-14 23:44:36 +0530

    web server setup first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Phila',
+        forecast: '50 degree celsius'
+    })
+})
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
